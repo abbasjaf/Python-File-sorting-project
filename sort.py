9034f90e7862752b06e985b3dbc068f703723f0d
Hermano <hermanohermana1@outlook.com>
2025-02-26 23:07:20 +0100

    refactored code, adding image hashing

--- sort.py
+++ sort.py
@@ -1,13 +1,13 @@
 from PIL import Image, UnidentifiedImageError
 from PIL.ExifTags import TAGS
+import imagehash
 from datetime import datetime
 from pathlib import Path
 import shutil
 import time
 import mimetypes
-from concurrent.futures import ThreadPoolExecutor
 
-parent_dir = Path(r"C:\Users\herma\Desktop\Copy")
+parent_dir = Path(r"C:\Users\herma\Desktop\copy")
 
 video_mime_types = [
     "video/mp4",         # MP4 (MPEG-4 Part 14)
@@ -53,42 +53,34 @@ mutagen_supported_audio_formats = [
     "mpc",   # Musepack
     "au",    # AU (Sun Audio)
 ]
-
-image_formats = list(map(str.lower, Image.registered_extensions().values()))
-image_formats.append("jpg")
-video_formats = [item.split("/")[1] for item in video_mime_types]
 audio_mime_types = ["audio/" + item for item in mutagen_supported_audio_formats]
 
 number_of_files_transferred: int = 0
 number_of_unknown_files: int = 0
+number_of_empty_folders_deleted: int = 0
+number_of_files: int = 0
 
-
-def image_metadata_extractor(source_file, image):
+    
+def file_metadata_extractor(source_file, image=None):
     """ Verify file is an image and extract the dates. Convert the str date object to an datetime object """
     
-    image_exifdata = image.getexif()
-    
-    if image_exifdata:
-        decoded_exif = {TAGS.get(tag, tag): value for tag, value in image_exifdata.items()}        
-        date_taken = decoded_exif.get("DateTime", decoded_exif.get("DateTimeOriginal"))
+    if image:
+        image_exifdata = image.getexif()
         
-        if date_taken:
-            return datetime.strptime(date_taken, "%Y:%m:%d %H:%M:%S")
-    else:
-        # For e.g screen shots that does not have exif data
-        mod_time = source_file.stat().st_mtime
-        date_taken = datetime.fromtimestamp(mod_time)
-        
-        if date_taken:
-            return date_taken
-
-def video_metadata_extractor(source_file):
-    mod_time = source_file.stat().st_mtime
-    date_taken = datetime.fromtimestamp(mod_time)
+        if image_exifdata:
+            decoded_exif = {TAGS.get(tag, tag): value for tag, value in image_exifdata.items()}        
+            date_taken = decoded_exif.get("DateTime", decoded_exif.get("DateTimeOriginal"))
+            
+            if date_taken:
+                return datetime.strptime(date_taken, "%Y:%m:%d %H:%M:%S")
+        else:
+            # For e.g screen shots that does not have exif data
+            mod_time = source_file.stat().st_mtime
+            date_taken = datetime.fromtimestamp(mod_time)
+            
+            if date_taken:
+                return date_taken
     
-    return date_taken
-
-def audio_metadata_extractor(source_file):
     mod_time = source_file.stat().st_mtime
     date_taken = datetime.fromtimestamp(mod_time)
     
@@ -101,7 +93,7 @@ def handle_unknown_files(file, source_file):
     
     # Skip makedir if dir exist
     unknown_dir.mkdir(exist_ok=True)
-    file_destination = unknown_dir / file.name
+    file_destination = unknown_dir / file
     
     if not file_destination.exists():
         shutil.copy2(source_file, unknown_dir)
@@ -117,58 +109,64 @@ def move_file_to_year_folder(file, source_file, file_date_obj):
         # Skip makedir if dir exist
         sorted_image_folders.mkdir(exist_ok=True)
         # Path to filed that is being moved
-        file_destination = sorted_image_folders / file.name
+        file_destination = sorted_image_folders / file
         
         if not file_destination.exists():
             shutil.copy2(source_file, sorted_image_folders)
             global number_of_files_transferred
             number_of_files_transferred += 1
 
-def process_files(file):
-    """ Based on the MIME-type, call different sorting functions """
-    
-    source_file = Path(file)                        
-    file_mime_type, _ = mimetypes.guess_type(source_file)
-    
-    try:
-        # Check if file type is an image
-        if file_mime_type and file_mime_type.startswith("image/"):
-            with Image.open(source_file) as image:
-                file_date_obj: datetime = image_metadata_extractor(source_file, image)
-                move_file_to_year_folder(file, source_file, file_date_obj)
-        # check for video files
-        elif file_mime_type in video_mime_types:
-            file_date_obj = video_metadata_extractor(source_file)
-            move_file_to_year_folder(file, source_file, file_date_obj)
-        elif file_mime_type in audio_mime_types:
-            file_date_obj = audio_metadata_extractor(source_file)
-            move_file_to_year_folder(file, source_file, file_date_obj)
-        else:
-            handle_unknown_files(file, source_file)
-    except UnidentifiedImageError as e:
-        handle_unknown_files(file, source_file)
-    except Exception as e:
-        handle_unknown_files(file, source_file)
-    
-def main():
-    files = list(parent_dir.rglob("*"))
-    number_of_files = len(files)
-    
-    with ThreadPoolExecutor(max_workers=4) as executor:
-        executor.map(process_files, files)
+def process_files():    
+    for dirpath, _, filenames in parent_dir.walk():
+        
+        # Delete folder if it's empty
+        if not any(dirpath.iterdir()):
+            dirpath.rmdir()
+            global number_of_empty_folders_deleted
+            number_of_empty_folders_deleted += 1
+            continue
+        
+        for file in filenames:
+            source_file = Path(dirpath / file)                        
+            file_mime_type, _ = mimetypes.guess_type(source_file)
+            
+            try:
+                # Check if file type is an image
+                if file_mime_type and file_mime_type.startswith("image/"):
+                    with Image.open(source_file) as image:
+                        file_date_obj: datetime = file_metadata_extractor(source_file, image)
+                        move_file_to_year_folder(file, source_file, file_date_obj)
+                
+                if file_mime_type:
+                    file_date_obj: datetime = file_metadata_extractor(source_file, image)
+                    move_file_to_year_folder(file, source_file, file_date_obj)
+                else:
+                    handle_unknown_files(file, source_file)
+            except UnidentifiedImageError:
+                handle_unknown_files(file, source_file)
+            except Exception as e:
+                handle_unknown_files(file, source_file)
     
     print(f"Number of files identified: {number_of_files}")
     print(f"Number of unknown files: {number_of_unknown_files}")
     print(f"Number of files transferred: {number_of_files_transferred}")
-        
+    print(f"Number of deleted empty folders: {number_of_empty_folders_deleted}")
+
+def hash_image():
+    img1 = imagehash.average_hash(Image.open(r"C:\Users\herma\Desktop\copy\Bilder från telefonen\DCIM\Camera\20170919_113950.jpg"))
+    img2 = imagehash.average_hash(Image.open(r"C:\Users\herma\Desktop\copy\Bilder från telefonen\DCIM\Camera\20170919_143709.jpg"))
+    
+    print(img1)
+    print(img2)
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
     
-    main()
+    # process_files()
+    hash_image()
     
     end_time = time.perf_counter()
     
     total_time = end_time - start_time
     
-    print(f"Total execution time: {total_time:2f} seconds")
+    print(f"Total execution time: {total_time:.2f} seconds")
